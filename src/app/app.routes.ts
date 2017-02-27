498ea95793690acf08b3ba550bc026990f1a7c66
andrew.nash <akahadaka@gmail.com>
2017-02-27 11:55:13 +0200

    Signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { Routes, RouterModule }         from '@angular/router';
 // app
 import { AppComponent }                 from './app.component';
 import { LoginComponent }               from './auth/login/login.component';
-import { MembersComponent }             from './auth/members/members.component';
-//import { SignupComponent }              from './auth/signup/signup.component';
+import { SignupComponent }              from './auth/signup/signup.component';
 import { EmailComponent }               from './auth/email/email.component';
+import { MembersComponent }             from './auth/members/members.component';
 
 // services
 //import { AuthGuard }                    from './auth/auth.service';
@@ -15,7 +15,7 @@ import { EmailComponent }               from './auth/email/email.component';
 export const router: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    //{ path: 'signup', component: SignupComponent },
+    { path: 'signup', component: SignupComponent },
     { path: 'login-email', component: EmailComponent },
     { path: 'members', component: MembersComponent }//, canActivate: [AuthGuard] }
 
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+// angular
+import { Component, OnInit }            from '@angular/core';
+import { Router }                       from '@angular/router';
+
+// libraries
+import { AngularFire }                  from 'angularfire2';
+import { AuthProviders, AuthMethods }   from 'angularfire2';
+
+// app
+import { moveIn, fallIn }               from '../../router.animations';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss'],
+    animations: [moveIn(), fallIn()],
+    host: {'[@moveIn]': ''}
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+    state: string = '';
+    error: any;
 
-  ngOnInit() {
-  }
+    constructor(public af: AngularFire, private router: Router) {  }
+
+    ngOnInit() {
+    }
+
+    onSubmit(formData) {
+        if (formData.valid) {
+            console.log(formData.value);
+            this.af.auth.createUser({
+                email: formData.value.email,
+                password: formData.value.password
+            }).then((success) => {
+                this.router.navigate(['/login']);
+            }).catch((err) => {
+                this.error = err;
+            });
+        }
+    }
+
+    clearErrors() {
+        this.error = '';
+    }
 
 }
